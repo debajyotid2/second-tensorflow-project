4bbe32a7ce2d2c93cd429e3d84ad6188a1a52ec0
debajyotid2 <92257044+debajyotid2@users.noreply.github.com>
2022-03-16 01:08:40 -0400

    added convolutional layers to model

--- cifar.py
+++ cifar.py
@@ -1,41 +1,53 @@
-import tensorflow as tf
-import numpy as np
+# code to handle tensorflow GPU errors
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    # Restrict TensorFlow to only use the first GPU
+    try:
+        tf.config.set_visible_devices(gpus[0], 'GPU')
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        # Visible devices must be set before GPUs have been initialized
+        print(e)
+
+# end code to handle tensorflow GPU errors
+
 
 # load dataset
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 # preprocess dataset
-X_train, X_test = X_train[:, :, :, 0], X_test[:, :, :, 0]
 X_train, X_test = X_train.astype(
     'float32')/255.0, X_test.astype('float32')/255.0
 
-# train, val, test split
-val_size = len(y_test)
-val_idx = np.random.randint(0, len(y_train), size=len(y_train))
-X_val, y_val = X_train[val_idx[:val_size]], y_train[val_idx[:val_size]]
-X_train, y_train = X_train[val_idx[val_size:]], y_train[val_idx[val_size:]]
+# one-hot encode y labels for use with categorical crossentropy
 
+one_hot_encoder = OneHotEncoder(sparse=False)
+y_train_encoded = one_hot_encoder.fit_transform(y_train)
 
 # build model
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(32, 32)),
-    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(3, 3)),
+    tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+    tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-# compile model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+# train model
+model.compile(optimizer='adam', loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # train model
-history_callback = model.fit(X_train, y_train, epochs=50)
+history_callback = model.fit(
+    X_train, y_train_encoded, batch_size=128, epochs=50, validation_split=0.2)
 loss_history, accuracy_history = history_callback.history[
     "loss"], history_callback.history["accuracy"]
-
-# validate model
-val_error, val_acc = model.evaluate(X_val, y_val)
-print(f"Valication accuracy = {val_acc*100:.3f} %")
 
 # test model
 preds = np.argmax(model.predict(X_test), axis=1)[:, np.newaxis]
@@ -56,4 +68,4 @@ plt.ylabel("Accuracy")
 plt.savefig("loss_accuracy.png", bbox_inches="tight", dpi=200)
 
 # save trained model
-model.save("cifar10.h5")
+model.save("cifar10_cnn.h5")
